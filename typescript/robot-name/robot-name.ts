7b9cb3f3aa9e780d848869d1f08041a6a2deb4f3
Zackary Allouche <zackary.allouche@oclock.school>
2026-07-04 17:26:43 +0200

    init robot-name exercise

---
+++ typescript/robot-name/robot-name.ts
@@ -0,0 +1,15 @@
+export class Robot {
+  constructor() {}
+
+  public get name(): string {
+    throw new Error('Remove this line and implement the function')
+  }
+
+  public resetName(): void {
+    throw new Error('Remove this line and implement the function')
+  }
+
+  public static releaseNames(): void {
+    throw new Error('Remove this line and implement the function')
+  }
+}
