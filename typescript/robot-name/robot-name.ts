660b8184ed16b9bb31fb514a050f9cba0ffcaf58
Zackary Allouche <zackary.allouche@oclock.school>
2026-07-04 22:35:55 +0200

    track used robot names to prevent duplicates

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,8 +1,9 @@
 export class Robot {
   private _name: string;
+  private static usedNames = new Set<string>();
 
   constructor() {
-    this._name = this.generateName();
+    this._name = this.generateUniqueName();
   }
 
   public get name(): string {
@@ -10,13 +11,14 @@ export class Robot {
   }
 
   public resetName(): void {
-    this._name = this.generateName();
+    this._name = this.generateUniqueName();
   }
 
   public static releaseNames(): void {
+    Robot.usedNames.clear();
   }
 
-private generateLetter(): string {
+  private generateLetter(): string {
     const randomCode = 65 + Math.floor(Math.random() * 26);
     return String.fromCharCode(randomCode);
   }
@@ -28,10 +30,19 @@ private generateLetter(): string {
   private generateName(): string {
     return (
       this.generateLetter() +
+      this.generateLetter() +
       this.generateNumber() +
       this.generateNumber() +
-      this.generateNumber() + 
       this.generateNumber()
-    )
+    );
+  }
+
+  private generateUniqueName(): string {
+    let newName = this.generateName();
+    while (Robot.usedNames.has(newName)) {
+      newName = this.generateName();
+    }
+    Robot.usedNames.add(newName);
+    return newName;
   }
 }
