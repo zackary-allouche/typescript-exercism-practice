702f5a30bfcf2c5b5a42cc753f5fbe50161e9b8f
Zackary Allouche <zackary.allouche@oclock.school>
2026-07-06 18:29:25 +0200

    complete word-count exercise

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,3 +1,12 @@
-export function count() {
+export function count(phrase: string): Map<string, number> {
+  const wordCounts = new Map<string, number>();
 
+  const words = phrase.toLowerCase().match(/[a-z0-9]+(?:'[a-z0-9]+)?/g) ?? [];
+
+  for (const word of words) {
+    const currentCount = wordCounts.get(word) ?? 0;
+    wordCounts.set(word, currentCount + 1);
+  }
+
+  return wordCounts;
 }
